9d6af389fb70713517f4d5603d54aa6d8dd01473
Taron Foxworth <taronfoxworth@gmail.com>
2017-07-21 09:48:43 -0400

    Initial commit

---
+++ fs/init.js
@@ -0,0 +1,24 @@
+load('api_config.js');
+load('api_gpio.js');
+load('api_mqtt.js');
+load('api_timer.js');
+
+// Set the pin number. On the NodeMCU D1 is pin 5.
+// See pinout: https://pradeepsinghblog.files.wordpress.com/2016/04/nodemcu_pins.png?
+let pirSensor = 5;
+
+// Blink built-in LED every second
+GPIO.set_mode(pirSensor, GPIO.MODE_INPUT);
+
+// Look for motion every 2 seconds.
+Timer.set(2000 /* 2 sec */ , true /* repeat */ , function () {
+  let motion = GPIO.read(pirSensor);
+  let topic = '/losant/' + Cfg.get('device.id') + '/state';
+  let message = JSON.stringify({
+    data: {
+      movement: motion
+    }
+  });
+  let ok = MQTT.pub(topic, message, 1);
+  print('Published:', ok ? 'yes' : 'no', 'topic:', topic, 'message:', message);
+}, null);
